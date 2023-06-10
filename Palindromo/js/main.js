6422c15dd323078f8d173d4b2d51c695f0bc42f3
Carmine Faella <carminefaella5@gmail.com>
2023-06-10 21:18:09 +0200

    Alcuni miglioramento al codice e abbellimento

--- Palindromo/js/main.js
+++ Palindromo/js/main.js
@@ -27,25 +27,40 @@ if(richiestaParola == richiestaParolaUnita){
 //2 metodo trovato
 //Definisco le variabili
 
-const richiestaParola = prompt('Inserisci una parola, verificheremo se sia palindroma');
-console.log(richiestaParola);
+const parolaInserita = document.querySelector('.parolaInserita');
+const button = document.querySelector('.button');
+const risultato = document.querySelector('.risultato');
+const resetta = document.querySelector('.reset');
+
+button.addEventListener('click', function () {
+
+  const richiestaParola = prompt('Inserisci una parola, verificheremo se sia palindroma');
+
+  parolaInserita.innerHTML += richiestaParola;
 
 let richiestaParolaInvertita = invertiParola(richiestaParola);
 
 //Predispongo la condizione per verificare la palindromia
 
-if(richiestaParola == richiestaParolaInvertita){
-    console.log('la parola è palindroma');
+  if (richiestaParola == richiestaParolaInvertita) {
+    risultato.innerHTML += 'la parola è palindroma';
   } else {
-    console.log('la parola non è palindroma');
+    risultato.innerHTML += 'la parola non è palindroma';
   }
 
-//Predispongo la funzione di inversione
-  
-function invertiParola(parola){
-  let parolaInversa = parola.split('').reverse().join('');  
-  return parolaInversa;
-}
+});
+
+resetta.addEventListener('click', function(){
+  parolaInserita.innerHTML = 'La parola inserita è, ';
+  risultato.innerHTML = 'Il risultato è che, ';
+})
+
+  //Predispongo la funzione di inversione
+
+  function invertiParola(parola) {
+    let parolaInversa = parola.split('').reverse().join('');
+    return parolaInversa;
+  }
 
 /*
 //3 metodo trovato
